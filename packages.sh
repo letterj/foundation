dfe93b949e8f1114fda203ea05b35bb04ca97643
Jay Payne <letterj@zlabpy64-lucid.localdomain>
2011-07-02 16:40:43 -0700

    Cleaned up package file

---
+++ packages.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+
+sudo apt-get update
+
+# General
+sudo apt-get install vim screen --yes --force-yes
+
+# Python
+sudo apt-get install python-setuptools python-nose --yes --force-yes
+
